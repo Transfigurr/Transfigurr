d35a8ac3b870f3f3b8743c282ba1bf11af9e01a3
alexmichaelkeith <alex.michael.keith@gmail.com>
2024-01-09 19:33:44 -0500

    update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import asyncio
 import os
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from starlette.responses import FileResponse
 from src.tasks import periodic, scan
+from pathlib import Path
 from src.api.utils import get_root_folder
-from fastapi.middleware.cors import CORSMiddleware
 from src.api.routes import (
     codec_routes, 
     profile_routes, 
@@ -32,24 +32,6 @@ routers = [
     websocket_routes.router
 ]
 
-# CORS
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8000",
-    "http://localhost:3000",
-]
-
-# Add CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 for router in routers:
     app.include_router(router)
 
@@ -68,8 +50,10 @@ async def startup_event():
     loop = asyncio.get_event_loop()
     loop.run_in_executor(None, periodic.start_watchdog, await get_root_folder() + '/series')
 
-# Serve frontend
-@app.get("/")
-@app.get("/{path:path}")
-async def index():
-    return FileResponse("frontend/build/index.html")
+# Catch all static routes
+@app.get("/{full_path:path}", response_class=HTMLResponse)
+async def read_item(request: Request, full_path: str):
+    file_path = Path(f"frontend/build/{full_path}")
+    if not file_path.exists() or file_path.is_dir():
+        file_path = Path("frontend/build/index.html")
+    return HTMLResponse(file_path.read_text(), media_type="text/html")
