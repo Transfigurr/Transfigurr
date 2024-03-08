a4694c23152857fb3b5e4a6f6c924a540dd86f9b
alexmichaelkeith <alex.michael.keith@gmail.com>
2024-03-08 10:14:10 -0500

    update season_routes

--- src/api/routes/season_routes.py
+++ src/api/routes/season_routes.py
@@ -1,20 +1,25 @@
 
 from fastapi import APIRouter, Depends
 from src.api.controllers.auth_controller import login_with_token
-from src.api.controllers.season_controller import get_all_seasons, get_season, set_season
+from src.api.controllers.season_controller import get_all_seasons, get_season, set_season, remove_season
 router = APIRouter()
 
 
-@router.get("/api/season", tags=["Season"])
+@router.get("/api/season", tags=["Season"], description="Get all Seasons")
 async def get_all_seasons_route(user: str = Depends(login_with_token)):
     return await get_all_seasons()
 
 
-@router.get("/api/season/{season_id}", tags=["Season"])
+@router.get("/api/season/{season_id}", tags=["Season"], description="Get Season by ID")
 async def get_season_route(season_id, user: str = Depends(login_with_token)):
     return await get_season(season_id)
 
 
-@router.put('/api/season/{season}', tags=["Season"])
+@router.put('/api/season/{season}', tags=["Season"], description="Upsert a season. If the season exists, it is updated. Otherwise, a new season is created.")
 async def set_season_route(season, user: str = Depends(login_with_token)):
     return await set_season(season)
+
+
+@router.delete("/api/season/{season_id}", tags=["Season"], description="Remove a Season by ID")
+async def remove_season_route(season_id, user: str = Depends(login_with_token)):
+    return await remove_season(season_id)
